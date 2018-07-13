67a6b7da5f443c1a3e6e9b9acb6b020ae40238a3
alendri <alendri@allbin>
2018-07-13 09:55:31 +0200

    Updated release scripts.

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,7 +2,6 @@ let del = require('del');
 let gulp = require('gulp4');
 let allbin = require('gulp-allbin');
 let tslint = require('gulp-tslint');
-
 let sourcemaps = require('gulp-sourcemaps');
 let ts = require('gulp-typescript');
 let tsProject = ts.createProject('./tsconfig.json');
@@ -16,7 +15,6 @@ function lint() {
         .pipe(tslint.report());
 }
 
-
 gulp.task('clean', () => {
     return del(['build', 'dist']);
 });
@@ -29,22 +27,8 @@ gulp.task('build', function () {
 });
 
 
-function tagDevAndPush(cb) {
-    try {
-        Promise.resolve()
-            .then(() => { return execPromise('git add package.json dist'); })
-            .then(() => { return execPromise('git commit -m "Release dev"'); })
-            .then(() => { return execPromise('git tag --force dev'); })
-            .then(() => { return execPromise('git push && git push --tags'); })
-            .then(() => { return cb(); });
-
-    } catch (err) {
-        return cb(err);
-    }
-}
-
 
 gulp.task('release:patch', gulp.series("clean", "build", allbin.tagAndPush(["package.json", "dist"], "patch")));
 gulp.task('release:minor', gulp.series("clean", "build", allbin.tagAndPush(["package.json", "dist"], "minor")));
 gulp.task('release:major', gulp.series("clean", "build", allbin.tagAndPush(["package.json", "dist"], "major")));
-gulp.task('release:dev', gulp.series("clean", "build", tagDevAndPush));
+gulp.task('release:dev', gulp.series("clean", "build", allbin.tagAndPush(["package.json", "dist"], "dev")));
